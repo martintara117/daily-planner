d06f96c5fa6ece7c9bb18f86180f905392becf9b
Tara Martin <taramartin@Taras-MacBook-Pro.local>
2020-10-01 10:29:49 -0400

    changed from military to am/pm. added init and put for loop in a function

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
+// needs to display the correct day and time inside the work day scheduler container
+// needs to display the time with am pm format
+// time goes from 9am to 5pm
+// attach style from css to vars
+// save button needs attribute with a lock icon
+// rows must turn grey if they are in the past
+// rows must turn red if they are present
+// rows must turn green if they are in the future
+// when save button is pressed it must commit the text to storage
+
 $(document).ready(function () {
   // DOM VARIABLES
-
-  for (i = 7; i < 18; i++) {
-    var row = $("<div>").attr("class", "row");
-    var time = $("<p>").text(i).attr("class", "col-1");
-    var text = $("<textarea>").attr("class", "col-10");
-    var btn = $("<button>").addClass("saveBtn").text("Save");
-    row.append(time);
-    row.append(text);
-    row.append(btn);
-    $(".container").append(row);
-  }
   // JS VARIABLES
   // FUNCTION DEFINITIONS
+  init();
+  function init() {
+    //call all functions that need to run once when page loads
+    buildScheduleHtml();
+    //colorcode schedule
+    //pull data from localStorage (if any)
+    //set current time in header
+  }
+  function buildScheduleHtml() {
+    let hours = [
+      "9AM",
+      "10AM",
+      "11AM",
+      "12PM",
+      "1PM",
+      "2PM",
+      "3PM",
+      "4PM",
+      "5PM",
+    ];
+    for (let hour of hours) {
+      var row = $("<div>").attr("class", "row");
+      var time = $("<p>").text(hour).attr("class", "col-1 hour");
+      var text = $("<textarea>").attr("class", "col-10");
+      var btn = $("<button>")
+        .attr("class", "saveBtn col-1")
+        .html("<i class='fa fa-lock'>");
+      row.append(time);
+      row.append(text);
+      row.append(btn);
+      $(".container").append(row);
+    }
+  }
   // FUNCTION CALLS
   // EVENT LISTENERS
 });
