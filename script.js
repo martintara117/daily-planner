6037b6659bc6a005af24be509bb503d20418d926
Tara Martin <taramartin@Taras-MacBook-Pro.local>
2020-10-07 17:53:56 -0400

    added event handlers that saved the text to local storage and changed the colors of the row based on what time of day it is. Grey if the event has passed, red if its presently time for that event, and green if its a future event.

--- script.js
+++ script.js
@@ -26,13 +26,16 @@ $(document).ready(function () {
   init();
   function init() {
     //call all functions that need to run once when page loads
-    //AND colorcode schedule
-    buildScheduleHtml();
     //pull data from localStorage (if any)
+    let dataObj = getData(); //{"9AM": "first cup of coffee", "10AM": "second cup"}
+    //AND colorcode schedule
+    buildScheduleHtml(dataObj);
     //set current time in header
     setHeaderTime();
+    //add event listeners for save button clicks
+    $(".saveBtn").on("click", handleClick);
   }
-  function buildScheduleHtml() {
+  function buildScheduleHtml(dataObj) {
     let currentHour = moment().format("ha").toUpperCase();
     let timeClass = "past";
     for (let hour of hours) {
@@ -40,7 +43,11 @@ $(document).ready(function () {
       else if (timeClass === "present") timeClass = "future";
       var row = $("<div>").attr("class", "row");
       var time = $("<p>").text(hour).attr("class", "col-1 hour");
-      var text = $("<textarea>").attr("class", "col-10 " + timeClass);
+      var text = $("<textarea>");
+      text.attr("class", "col-10 " + timeClass);
+      if (dataObj[hour]) {
+        text.val(dataObj[hour]);
+      }
       var btn = $("<button>")
         .attr("class", "saveBtn col-1")
         .html("<i class='fa fa-lock'>");
@@ -54,5 +61,25 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
   }
   //
-  // EVENT LISTENERS
+  // EVENT HANDLERS
+  function handleClick(e) {
+    //get data
+    let dataObj = getData();
+    //get parent row
+    let row = $(e.target).parents(".row")[0]; //should give us <div class="row"> that contains the <button> clicked
+    //get hour
+    let hour = $(row).find("p").text(); //"9AM" or "3PM"
+    //get text
+    let text = $(row).find("textarea").val(); //"first cup of coffee"
+    //set data
+    dataObj[hour] = text;
+    //save data
+    localStorage.setItem("schedule", JSON.stringify(dataObj));
+  }
+  //DATA HANDLING
+  function getData() {
+    let dataStr = localStorage.getItem("schedule") || "{}";
+    let dataObj = JSON.parse(dataStr);
+    return dataObj;
+  }
 });
